96b4cc48cfc1308760ae5f5f2634d18f304f05fb
jakemello <ductran15601@gmail.com>
2020-04-05 02:15:39 +0700

    added pug and template engine

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('views' , path.join(__dirname, "/views"));
+app.set('view engine', 'pug');
 
 const port = 3000;
 
 app.get('/', (req,res) => {
-    res.send("Success");
+    const dataArray = [
+        {name: "Duc Tran", age: 18},
+        {name: "Duc Huy", age:8}
+    ];
+    res.render("index",{
+        users: dataArray
+    });
 });
 
 app.listen(port, () => {
